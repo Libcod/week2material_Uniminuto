fc63b624f3c5b638aba6ba843c0e10c3254cf3b5
Libardo Salamanca Jimenez <lsalama9@uniminuto.edu.co>
2023-05-21 10:02:14 -0500

    Asignacion de botones de interaccion modulo usuarios

--- src/app/components/dashboard/usuarios/usuarios.component.ts
+++ src/app/components/dashboard/usuarios/usuarios.component.ts
@@ -1,24 +1,15 @@
 import { Component } from '@angular/core';
+import {Usuario} from "../../../interfaces/usuario";
 
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+const listUsuarios: Usuario[] = [
+  {usuario: 'admin', nombre: 'admin', apellido: 'admin', sexo: '-'},
+  {usuario: 'lsalamanca', nombre: 'libardo', apellido: 'salamanca', sexo: 'M'},
+  {usuario: 'psantos', nombre: 'pedro', apellido: 'santos', sexo: 'M',},
+  {usuario: 'csalazar', nombre: 'claudia', apellido: 'salazar', sexo: 'F'},
+
+
 ];
 
 @Component({
@@ -27,6 +18,6 @@ const ELEMENT_DATA: PeriodicElement[] = [
   styleUrls: ['./usuarios.component.css']
 })
 export class UsuariosComponent {
-  displayedColumns: string[] = ['Usuario', 'Nombre', 'Apellido', 'Sexo'];
-  dataSource = ELEMENT_DATA;
+  displayedColumns: string[] = ['Usuario', 'Nombre', 'Apellido', 'Sexo','Acciones'];
+  dataSource = listUsuarios;
 }
